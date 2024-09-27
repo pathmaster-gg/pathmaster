b96b236e0897976c4e5e0234efdabb667b2bffb7
Jonathan <152833616+jon-sait@users.noreply.github.com>
2024-09-27 18:45:42 +0800

    Login with Google button

---
+++ web/src/lib/constants/env.ts
@@ -0,0 +1,10 @@
+export const SERVER_URL: string =
+  process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:8787";
+
+export const OAUTH_GOOGLE_CLIENT_ID: string =
+  process.env.NEXT_PUBLIC_OAUTH_GOOGLE_CLIENT_ID ??
+  "456115587652-9o20ppfdr2fs6bgtcrg3d5g50j6o653c.apps.googleusercontent.com";
+
+export function getServerUrl(path: string): string {
+  return `${SERVER_URL}${path}`;
+}
