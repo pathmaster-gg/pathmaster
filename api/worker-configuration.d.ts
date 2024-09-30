e1f0b6e0c73058673d63dbffab170b260af5b053
Jonathan <152833616+jon-sait@users.noreply.github.com>
2024-09-30 20:11:09 +0800

    Add Cloudflare D1 binding

--- api/worker-configuration.d.ts
+++ api/worker-configuration.d.ts
@@ -4,4 +4,5 @@ interface Env {
   OAUTH_GOOGLE_CLIENT_ID: "456115587652-9o20ppfdr2fs6bgtcrg3d5g50j6o653c.apps.googleusercontent.com";
   OAUTH_GOOGLE_CLIENT_SECRET: "";
   OAUTH_GOOGLE_REDIRECT_URL: "http://localhost:3000/login/google";
+  DB: D1Database;
 }
