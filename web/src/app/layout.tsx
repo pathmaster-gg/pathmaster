bf6eadb81f83245e9f9ebb8afee3fd64f889ba32
Aylardex <132924342+Aylardex@users.noreply.github.com>
2024-10-02 22:10:13 -0600

    Fix background image

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { IdentityProvider } from "./lib/context/identity";
 
+import Background from "@/images/background.png";
+
 export const metadata: Metadata = {
   title: "PathMaster",
   description: "Your trusted companion app for managing Pathfinder",
@@ -21,7 +23,15 @@ export default function RootLayout({
           url(&#x27;https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;400;500;600;700;800;900&amp;display=swap&#x27;);
         </style>
       </head>
-      <body className="inknut-antiqua-regular antialiased">
+      <body
+        className="inknut-antiqua-regular antialiased"
+        style={{
+          backgroundImage: `url(${Background.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
         <IdentityProvider>{children}</IdentityProvider>
       </body>
     </html>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -11,7 +11,6 @@ import LinkButton from "@/components/link-button";
 import SocialButton from "@/components/social-button";
 
 import LogoPf2e from "@/images/logo_pf2e.png";
-import Background from "@/images/background.png";
 import JournalSvg from "@/images/icons/journals.svg";
 import BoxesSvg from "@/images/icons/boxes.svg";
 import DiscordSvg from "@/images/icons/discord.svg";
@@ -23,42 +22,32 @@ export default function Home() {
   const identity = useContext(IdentityContext);
 
   return (
-    <div>
-      <Image
-        src={Background}
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        alt="Background"
-        className="z-[-100]"
-      />
-      <div className="flex-col px-12 py-8">
-        <div className="flex items-center justify-between mb-12">
-          <span className="text-xl">ver. 0.1.0</span>
-          {identity.accountInfo ? (
-            <p>Welcome back, {identity.accountInfo.username}</p>
-          ) : (
-            <SignWithGoogleButton />
-          )}
+    <div className="flex-col px-12 py-8">
+      <div className="flex items-center justify-between mb-12">
+        <span className="text-xl">ver. 0.1.0</span>
+        {identity.accountInfo ? (
+          <p>Welcome back, {identity.accountInfo.username}</p>
+        ) : (
+          <SignWithGoogleButton />
+        )}
+      </div>
+      <div className="flex flex-col items-center">
+        <LogoHero />
+        <Dividor />
+        <p className="text-xl">Your trusted companion app for managing</p>
+        <Image priority src={LogoPf2e} width={430} alt="Pathfinder 2e logo" />
+        <div className="flex mt-16 gap-x-16">
+          <LinkButton href="/wiki" icon={JournalSvg} text="Rules & Guides" />
+          <LinkButton href="/modules" icon={BoxesSvg} text="Adventure Hub" />
         </div>
-        <div className="flex flex-col items-center">
-          <LogoHero />
-          <Dividor />
-          <p className="text-xl">Your trusted companion app for managing</p>
-          <Image priority src={LogoPf2e} width={430} alt="Pathfinder 2e logo" />
-          <div className="flex mt-16 gap-x-16">
-            <LinkButton href="/wiki" icon={JournalSvg} text="Rules & Guides" />
-            <LinkButton href="/modules" icon={BoxesSvg} text="Adventure Hub" />
-          </div>
-          <div className="flex mt-12 gap-x-8">
-            <SocialButton href="https://discord.com/" icon={DiscordSvg} />
-            <SocialButton href="https://twitter.com/" icon={TwitterSvg} />
-            <SocialButton href="https://facebook.com/" icon={FacebookSvg} />
-            <SocialButton
-              href="https://github.com/pathmaster-gg"
-              icon={GithubSvg}
-            />
-          </div>
+        <div className="flex mt-12 gap-x-8">
+          <SocialButton href="https://discord.com/" icon={DiscordSvg} />
+          <SocialButton href="https://twitter.com/" icon={TwitterSvg} />
+          <SocialButton href="https://facebook.com/" icon={FacebookSvg} />
+          <SocialButton
+            href="https://github.com/pathmaster-gg"
+            icon={GithubSvg}
+          />
         </div>
       </div>
     </div>
