1b949c725e4501571a150c8dfdbe383b0806c970
Jonathan <152833616+jon-sait@users.noreply.github.com>
2024-11-01 17:56:22 +0800

    Logout feature

--- web/src/components/profile.tsx
+++ web/src/components/profile.tsx
@@ -1,4 +1,8 @@
+import { useContext, useState } from "react";
+import { useRouter } from "next/navigation";
+
 import { AccountInfo } from "@/lib/models";
+import { IdentityContext } from "@/app/lib/context/identity";
 
 import ExampleAvatar from "@/images/example_avatar.png";
 import Link from "next/link";
@@ -8,9 +12,23 @@ interface IProps {
 }
 
 export default function Profile(props: IProps) {
+  const router = useRouter();
+
+  const identity = useContext(IdentityContext);
+
+  const [popupOpen, setPopupOpen] = useState<boolean>(false);
+
+  const handleLogout = () => {
+    identity.setSession(undefined);
+    router.push("/");
+  };
+
   return (
-    <Link href="/dashboard" className="flex min-h-16">
-      <div className="flex items-center px-8 gap-4 cursor-pointer hover:bg-white/10">
+    <div className="relative flex flex-col items-center">
+      <div
+        className="z-50 h-full min-h-16 flex items-center px-8 gap-4 cursor-pointer hover:bg-white/10"
+        onClick={() => setPopupOpen(!popupOpen)}
+      >
         <div
           className="w-10 h-10 border border-highlight rounded-full"
           style={{
@@ -19,8 +37,29 @@ export default function Profile(props: IProps) {
             backgroundSize: "cover",
           }}
         ></div>
-        <span className="text-sm">{props.account.username}</span>
+        <span className="text-sm select-none">{props.account.username}</span>
       </div>
-    </Link>
+      {popupOpen && (
+        <div
+          className="z-40 fixed w-full h-full top-0 left-0"
+          onClick={() => setPopupOpen(false)}
+        ></div>
+      )}
+      {popupOpen && (
+        <div className="z-50 absolute box-border w-full max-w-48 -bottom-32 bg-background flex flex-col border border-highlight">
+          <Link href="/dashboard" onClick={() => setPopupOpen(false)}>
+            <div className="px-4 py-4 select-none hover:bg-white/10 active:bg-white/15">
+              Dashboard
+            </div>
+          </Link>
+          <div
+            className="px-4 py-4 select-none cursor-pointer hover:bg-white/10 active:bg-white/15"
+            onClick={handleLogout}
+          >
+            Logout
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
