8c95a30a41f0aaa3908a06bc535dbeab404fd88a
Jonathan <152833616+jon-sait@users.noreply.github.com>
2024-11-01 18:30:59 +0800

    Create more mock adventures

--- mock/init.sh
+++ mock/init.sh
@@ -15,6 +15,30 @@ curl "https://pathmaster.gg/api/image/adventure_cover" \
   -H "Authorization: Bearer $TOKEN" \
   --data-binary @images/cover_kingmaker.jpg
 
+curl "https://pathmaster.gg/api/image/adventure_cover" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  --data-binary @images/cover_hellknight_hill.jpg
+
+curl "https://pathmaster.gg/api/image/adventure_cover" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  --data-binary @images/cover_prey_for_death.jpg
+
+curl "https://pathmaster.gg/api/image/adventure_cover" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  --data-binary @images/cover_ruins_of_gauntlight.jpg
+
+curl "https://pathmaster.gg/api/image/adventure_cover" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  --data-binary @images/cover_the_great_toys_heist.jpg
+
 curl "https://pathmaster.gg/api/adventure" \
   --fail \
   -X POST \
@@ -23,4 +47,44 @@ curl "https://pathmaster.gg/api/adventure" \
   --data '{
   "name": "Kingmaker",
   "cover_image_id": 1
+}'
+
+curl "https://pathmaster.gg/api/adventure" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "Content-Type: application/json" \
+  --data '{
+  "name": "Hellknight Hill",
+  "cover_image_id": 2
+}'
+
+curl "https://pathmaster.gg/api/adventure" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "Content-Type: application/json" \
+  --data '{
+  "name": "Prey for Death",
+  "cover_image_id": 3
+}'
+
+curl "https://pathmaster.gg/api/adventure" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "Content-Type: application/json" \
+  --data '{
+  "name": "Ruins of Gauntlight",
+  "cover_image_id": 4
+}'
+
+curl "https://pathmaster.gg/api/adventure" \
+  --fail \
+  -X POST \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "Content-Type: application/json" \
+  --data '{
+  "name": "The Great Toys Heist",
+  "cover_image_id": 5
 }'
